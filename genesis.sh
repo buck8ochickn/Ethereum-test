1e72f92f6bda24540897a2cd65457e2577702e0e
buck8ochickn <nickgarner2050@gmail.com>
2016-01-17 23:26:06 -0500

    test net

---
+++ genesis.sh
@@ -0,0 +1,1 @@
+geth --identity "MyNodeName" --genesis ~/Ethereum/Skynet/customgenesis.json --rpc --rpcport "8545" --rpccorsdomain "*" --datadir ~/Ethereum/Skynet/datadir --port "30342" --nodiscover --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "db,eth,net,web3" --autodag --networkid 1900 --nat "any" console
